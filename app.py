7ae54d0e65360e9678b98278882df0c78d8dd3ff
Shweta <arunbasappagudasi@Aruns-MacBook-Pro.local>
2020-07-23 18:07:06 -0700

    created different response list for each user by adding response list in session, and flashing a msg and displaying the right question if user tries to manually change question number in url

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ app.config['SECRET_KEY'] = 'this-is-secret'
 
 debug = DebugToolbarExtension(app)
 
-responses = []
-
 
 @app.route('/')
 def survey_welcome_page():
@@ -19,16 +17,29 @@ def survey_welcome_page():
     return render_template('welcome.html', survey = satisfaction_survey)
 
 
+@app.route('/questions/<int:question_no>', methods=["POST"])
+def create_responseList(question_no):
+    '''creates response list in session for each user'''
+
+    session['response'] = []
+    return redirect('/questions/0')
+
+
+
 @app.route('/questions/<int:question_no>')
 def display_question(question_no):
     '''Prompts the user to answer each question contained in the survey instance'''
 
-    question_number = question_no
-    
-    print(f"i am inside questions/0")
-    
-    session['question_number'] = question_number
+    ##question_number = question_no
 
+    print(f"i am inside questions/0")
+    if question_no == 0:
+        session['question_number'] = 0
+    elif question_no != session['question_number'] :
+        flash("You’re trying to access an invalid question")
+        question_no = session['question_number']
+
+    session['question_number'] = question_no
     return render_template('question.html', question = satisfaction_survey.questions[question_no],  question_number = session['question_number'])
 
 
@@ -36,13 +47,11 @@ def display_question(question_no):
 def collect_answers():
     '''Collects the user's response to the survey question by appending their answer into the responses list. Redirects the user to the next question'''
 
-    responses.append(request.form['choice'])
+    session['response'].append(request.form['choice'])
     session['question_number'] += 1
     next_question = session['question_number']
 
-    print(responses)
     if next_question < len(satisfaction_survey.questions):
         return redirect(f'/questions/{next_question}')
-    
-    else:
-        return redirect('/thank-you.html')
+    else :
+        return render_template('/thank-you.html')
